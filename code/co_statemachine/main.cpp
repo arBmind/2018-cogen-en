18d09b8609600e756ce80de1819b71a975aa7026
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2018-11-18 15:21:23 +0100

    improved state machine outputs

--- code/co_statemachine/main.cpp
+++ code/co_statemachine/main.cpp
@@ -2,26 +2,54 @@
 
 #include <iostream>
 
-auto operator<<(std::ostream &o, Elevator::Move m)
+using namespace Elevator;
+
+auto operator<<(std::ostream &o, Move m) -> std::ostream & {
+  return o << (m == Move::Up ? "Up" : "Down");
+}
+
+auto operator<<(std::ostream &o, Event e)
     -> std::ostream & {
-    using Move = Elevator::Move;
-    return o << (m == Move::Up ? "Up" : "Down");
+  auto handlers = overloaded{
+      [&](Called c) -> std::ostream & {
+        return o << "Called to: " << c.toFloor;
+      },
+      [&](FloorSensor fs) -> std::ostream & {
+        return o << "FloorSensor at: " << fs.floor;
+      },
+      [&](AlarmPressed) -> std::ostream & {
+        return o << "AlarmPressed";
+      } //
+  };
+  return std::visit(handlers, e);
+}
+
+auto operator<<(std::ostream &o, State s)
+    -> std::ostream & {
+
+  auto handlers = overloaded{
+      [&](Idle) -> std::ostream & { return o << "Idle"; },
+      [&](Broken) -> std::ostream & {
+        return o << "Broken";
+      },
+      [&](Move m) -> std::ostream & {
+        return o << "Move: " << m;
+      } //
+  };
+  return std::visit(handlers, s);
 }
 
 int main() {
-    auto elevator = Elevator::create();
-    auto &cout = std::cout;
-    using namespace Elevator;
+  auto elevator = Elevator::create();
+  auto &o = std::cout;
 
-    for (auto event : std::initializer_list<Event>{
-             Called{2}, FloorSensor{1}, FloorSensor{2},
-             Called{1}, FloorSensor{1}, AlarmPressed{}}) {
+  o << "@ " << elevator.state() << '\n';
+  for (auto event : std::initializer_list<Event>{
+           Called{2}, FloorSensor{1}, FloorSensor{2},
+           Called{1}, FloorSensor{1}, AlarmPressed{}}) {
 
-        elevator.handle(event);
-        match(
-            pattern(elevator.state()),
-            [&](Idle) { cout << "Idle\n"; },
-            [&](Broken) { cout << "Broken\n"; },
-            [&](Move m) { cout << "Move: " << m << '\n'; });
-    }
+    o << "+ " << event << '\n';
+    elevator.handle(event);
+    o << "@ " << elevator.state() << '\n';
+  }
 }
